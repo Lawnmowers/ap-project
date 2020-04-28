131ead58044f416476947cfff3aab4f4d6a8199e
Lawnmowers <42724546+Lawnmowers@users.noreply.github.com>
2020-04-28 14:37:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
+var numberOfTasks = 0;
+
 function addTask(form) {
-    if (form.taskInput){
+    if (form.taskInput && form.taskInput.value !== ""){
         var newChild = document.createElement("li");
-        var divider = document.createElement("hr");
+        var newButton = document.createElement("button");
         var referenceNode = document.getElementById("referenceNode");
 
+        newChild.style.margin = "20px 10px";
         newChild.innerHTML = form.taskInput.value;
+        newButton.innerText = "Delete";
+
+        newButton.setAttribute("class", "deleteButton");
+        newChild.setAttribute("class", "listItem");
+
         document.getElementById("taskList").insertBefore(newChild, referenceNode);
-        document.getElementById("taskList").insertBefore(divider, referenceNode);
+        document.getElementsByClassName("listItem")[numberOfTasks].appendChild(newButton);
+
+        numberOfTasks++;
         form.reset();
     } 
+}
+
+//fix delete button
+newButton.addEventListener("click", deleteTask(0, newChild));
+
+function deleteTask(taskNumber, taskItem) {
+    taskItem[taskNumber].parentNode.removeChild(taskItem[taskNumber]);
 }
