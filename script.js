2ac952a79ac248b719226d0d89658231ad54d551
Lawnmowers <42724546+Lawnmowers@users.noreply.github.com>
2020-05-01 21:35:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,54 @@
-var numberOfTasks = 0;
+var taskList = document.getElementById("taskList");
+var taskNumber = 0;
+
+submitButton.addEventListener("click", addTask(form));
+
 
 function addTask(form) {
-    if (form.taskInput && form.taskInput.value !== ""){
+    if (form.taskInput && form.taskInput.value !== "") {
         var newChild = document.createElement("li");
         var newButton = document.createElement("button");
         var referenceNode = document.getElementById("referenceNode");
 
         newChild.style.margin = "20px 10px";
-        newChild.innerHTML = form.taskInput.value;
-        newButton.innerText = "Delete";
 
         newButton.setAttribute("class", "deleteButton");
         newChild.setAttribute("class", "listItem");
 
-        document.getElementById("taskList").insertBefore(newChild, referenceNode);
-        document.getElementsByClassName("listItem")[numberOfTasks].appendChild(newButton);
+        newButton.innerText = "Delete";
+        newChild.innerHTML = form.taskInput.value;
 
-        numberOfTasks++;
+        taskList.insertBefore(newChild, referenceNode);
+        document.getElementsByClassName("listItem")[taskNumber].appendChild(newButton);
+       
+        taskNumber++;
         form.reset();
     } 
 }
 
-//fix delete button
-newButton.addEventListener("click", deleteTask(0, newChild));
+taskList.addEventListener("click", deleteButton);
 
-function deleteTask(taskNumber, taskItem) {
-    taskItem[taskNumber].parentNode.removeChild(taskItem[taskNumber]);
+function deleteButton(event) {
+    if (event.target.className !== "deleteButton") {
+        return;
+    }
+
+    taskNumber--;
+    event.target.parentElement.remove();
+}
+
+taskList.addEventListener("click", completeTask);
+
+function completeTask(event) {
+    if (event.target.matches("button") || event.target.className !== "listItem") {
+        return;
+    }
+
+    if (event.target.style.opacity == 1) {
+        event.target.style.opacity = 0.5;
+        event.target.style.textDecoration = "line-through";
+    } else {
+        event.target.style.opacity = 1;
+        event.target.style.textDecoration = "none";
+    }
 }
